9cdaa56a6ec9e4c2901cf004fd50c5dd200d3618
Siddesh Patil <87792589+Coolasid@users.noreply.github.com>
2021-12-11 18:52:38 -0500

    working on slider

--- JS Files/HomePage.js
+++ JS Files/HomePage.js
@@ -117,3 +117,109 @@ function saveIBox() {
     f++;
 
 }
+
+
+// Slider=>
+
+let apProd = document.getElementById("aProducts");
+
+var sliderArr = [
+
+    {
+        img:"https://pisces.bbystatic.com/image2/BestBuy_US/images/products/6401/6401049_sd.jpg;maxHeight=272;maxWidth=400",
+        name: "Sony - 75 Class XBR X800H Series LED 4K UHD Smart Android TV",
+        rating: "4.5",
+        price: "$1,234.99"
+    },
+
+    {
+        img: "https://pisces.bbystatic.com/image2/BestBuy_US/images/products/6331/6331591_sd.jpg;maxHeight=272;maxWidth=400",
+        name: "Sony - 55 Class A9G MASTER Series OLED 4K UHD Smart Android TV",
+        rating: "4.7",
+        price: "$2,299.99"
+    },
+
+    {
+        img: "https://pisces.bbystatic.com/image2/BestBuy_US/images/products/6401/6401199_sd.jpg;maxHeight=272;maxWidth=400",
+        name: "Sony - 55 Class A8H Series OLED 4K UHD Smart Android TV",
+        rating: "5.0",
+        price: "$1,899.99"
+    },
+
+    {
+        img: "https://pisces.bbystatic.com/image2/BestBuy_US/images/products/6455/6455215_sd.jpg;maxHeight=272;maxWidth=400",
+        name: "Sony - 75 Class XBR X800H Series LED 4K UHD Smart Android TV",
+        rating: "4.8",
+        price: "$2,999.99"
+    },
+
+    {
+        img: "https://pisces.bbystatic.com/image2/BestBuy_US/images/products/6401/6401206_sd.jpg;maxHeight=272;maxWidth=400",
+        name: "Sony - 65 Class X950H Series LED 4K UHD Smart Android TV",
+        rating: "4.9",
+        price: "$1,424.99"
+    },
+
+    {
+        img: "https://pisces.bbystatic.com/image2/BestBuy_US/images/products/6401/6401049_sd.jpg;maxHeight=272;maxWidth=400",
+        name: "Samsung - 70 Class TU6985 4K Crystal UHD Smart Tizen TV",
+        rating: "4.5",
+        price: "$599.99"
+    },
+
+    {
+        img: "https://pisces.bbystatic.com/image2/BestBuy_US/images/products/6401/6401049_sd.jpg;maxHeight=272;maxWidth=400",
+        name: "Sony - 75 Class XBR X800H Series LED 4K UHD Smart Android TV",
+        rating: "4.5",
+        price: "$1,234.99"
+    },
+
+    {
+        img: "https://pisces.bbystatic.com/image2/BestBuy_US/images/products/6331/6331597_sd.jpg;maxHeight=272;maxWidth=400",
+        name: "Sony - 65 Class A9G MASTER Series OLED 4K UHD Smart Android TV",
+        rating: "4.5",
+        price: "$2,999.99"
+    },
+
+    {
+        img: "https://pisces.bbystatic.com/image2/BestBuy_US/images/products/6401/6401049_sd.jpg;maxHeight=272;maxWidth=400",
+        name: "Sony - 75 Class XBR X800H Series LED 4K UHD Smart Android TV",
+        rating: "4.5",
+        price: "$1,234.99"
+    },
+
+    {
+        img: "https://pisces.bbystatic.com/image2/BestBuy_US/images/products/6455/6455217_sd.jpg;maxHeight=272;maxWidth=400",
+        name: "Sony - 55 Class BRAVIA XR A80J Series OLED 4K UHD Smart Google TV",
+        rating: "4.5",
+        price: "$1,699.99"
+    },
+
+    {
+        img: "https://pisces.bbystatic.com/image2/BestBuy_US/images/products/6401/6401049_sd.jpg;maxHeight=272;maxWidth=400",
+        name: "Sony - 75 Class XBR X800H Series LED 4K UHD Smart Android TV",
+        rating: "4.5",
+        price: "$1,234.99"
+    },
+
+    {
+        img: "https://pisces.bbystatic.com/image2/BestBuy_US/images/products/6401/6401718_sd.jpg;maxHeight=272;maxWidth=400",
+        name: "Samsung - 65 Class 8 Series LED 4K UHD Smart Tizen TV",
+        rating: "4.9",
+        price: "$749.99"
+    },
+
+    {
+        img: "https://pisces.bbystatic.com/image2/BestBuy_US/images/products/6453/6453321_sd.jpg;maxHeight=272;maxWidth=400",
+        name: "LG - 65 Class A1 Series OLED 4K UHD Smart webOS TV",
+        rating: "4.5",
+        price: "$1,499.99"
+    },
+
+
+]
+
+
+for(var i = 0; i < 6; i++){
+    
+}
