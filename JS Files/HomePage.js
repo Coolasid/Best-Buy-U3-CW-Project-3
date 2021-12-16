9e25354c13fa43a41033e87b40df8e81d21cad80
Siddesh Patil <87792589+Coolasid@users.noreply.github.com>
2021-12-16 21:56:50 +0530

    menu funct done

--- JS Files/HomePage.js
+++ JS Files/HomePage.js
@@ -33,7 +33,8 @@ function showMenuBox(){
     c++;
     
 }
-// showMenuBox()
+
+// ///////showMenuBox()
 
 let outerBox1 = document.getElementById("outerBox1");
 outerBox1.addEventListener("click",showNext);
@@ -58,33 +59,20 @@ function showNext(){
     
 }
 
-let outerB = document.getElementById("outerBox2");
-outerB.addEventListener("click", showNext1);
-var v = 0;
-function showNext1() {
-    let ch1 = document.getElementById("ch1")
+let ap = document.getElementById("ap");
+ap.addEventListener("click",()=>{
+    addLaptopData()
     
-    // outerBox1.style.backgroundColor = "#0046c4"
-    let sup = document.getElementById("Support");
-    if (v % 2 == 0) {
-        sup.style.display = "block";
-        outerB.style.backgroundColor = "#0046c4"
-        ch1.style.color = "white";
-    }
+})
 
-    if (v % 2 == 1) {
-        sup.style.display = "none";
-        outerB.style.backgroundColor = "white"
-        ch1.style.color = "#0046c4";
-        
-        
-    }
-    v++;
-
-}
+let tv5 = document.getElementById("tv");
+tv5.addEventListener("click",()=>{
+    addTvData()
+})
 
 
 
+///////////////
 
 // holidayPoP=>
 
